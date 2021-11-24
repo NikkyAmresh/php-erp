90851ab1620b06c60ea489cb1af801e6195ee8dd
Nikky Amresh <nikkyamresh8@gmail.com>
2021-11-24 17:06:17 +0530

    added reusable user login page

--- App/Controllers/Auth/Logout.php
+++ App/Controllers/Auth/Logout.php
@@ -6,12 +6,23 @@ use App\Helpers\Constants;
 use \App\Helpers\Session;
 
 class Logout extends \Core\Controller
+
 {
 
     public function indexAction()
     {
-        Session::reset();
-        
-        $this->redirect('/', ['type' => Constants::SUCCESS, 'message' => 'Logout Success!']);
+        if (Session::get(Constants::LOGGED_IN_STUDENT_USER_ID)) {
+            Session::reset();
+
+            $this->redirect('/student', ['type' => Constants::SUCCESS, 'message' => 'Logout Success!']);
+        } elseif (Session::get(Constants::LOGGED_IN_TEACHER_USER_ID)) {
+            Session::reset();
+
+            $this->redirect('/teacher', ['type' => Constants::SUCCESS, 'message' => 'Logout Success!']);
+        } elseif (Session::get(Constants::LOGGED_IN_ADMIN_ID)) {
+            Session::reset();
+
+            $this->redirect('/admin', ['type' => Constants::SUCCESS, 'message' => 'Logout Success!']);
+        }
     }
 }
