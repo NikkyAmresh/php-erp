2861d0b484a39990d7cbb56478d967b1889c8641
Nikky Amresh <Nikkyamresh8@gmail.com>
2021-12-23 13:29:18 +0530

    added selenium server to runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -1,1 +1,14 @@
+SELENIUM_SERVER="java -jar Tests/selenium-server-standalone-2.46.0.jar"
+
+echo "Starting Selenium Server on port 4444"
+$SELENIUM_SERVER > /dev/null 2>&1 &
+
+SELENIUM_SERVER_PID=$!
+sleep 1
+
 ./vendor/bin/phpunit --testdox Tests/Unit/Selenium/HomePage.php
+
+
+kill $SELENIUM_SERVER_PID
+
+echo "Selenium server Ended"
